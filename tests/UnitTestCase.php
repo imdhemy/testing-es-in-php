73fd39086f38297453a14ad3e3709638173e0bbf
imdhemy_cb <imdhemy@gmail.com>
2021-09-10 17:32:13 +0200

    add mockClient method to allow mocking responses

--- tests/UnitTestCase.php
+++ tests/UnitTestCase.php
@@ -2,9 +2,25 @@
 
 namespace ACME\Tests;
 
+use ACME\Tests\Utils\MockHandler;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 class UnitTestCase extends PHPUnitTestCase
 {
+    /**
+     * @param string $template
+     * @param array $overrides
+     * @param array $options
+     * @return Client
+     */
+    protected function mockClient(string $template, array $overrides = [], array $options = []): Client
+    {
+        $handler = MockHandler::mockTemplate($template, $overrides, $options);
+        $builder = ClientBuilder::create();
+        $builder->setHandler($handler);
 
+        return $builder->build();
+    }
 }

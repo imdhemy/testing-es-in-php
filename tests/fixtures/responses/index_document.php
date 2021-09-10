5a9a37e0ccf2b6858a712a733884eebf6d319a20
imdhemy_cb <imdhemy@gmail.com>
2021-09-10 19:19:57 +0200

    add index_document template

---
+++ tests/fixtures/responses/index_document.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    '_index' => 'quotes_index',
+    '_type' => '_doc',
+    '_id' => 'r7u30HsBzf-HgZ0_Prvo',
+    '_version' => 1,
+    'result' => 'created',
+    '_shards' =>
+        [
+            'total' => 2,
+            'successful' => 1,
+            'failed' => 0,
+        ],
+    '_seq_no' => 1,
+    '_primary_term' => 1,
+];
